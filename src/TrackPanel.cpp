072744665f3581e87177ca18d4c1b4621808cf53
Paul Licameli <paul.licameli@gmail.com>
2015-06-09 16:21:29 -0400

    Remove Envelope::GetValueAtX

--- src/TrackPanel.cpp
+++ src/TrackPanel.cpp
@@ -7011,8 +7011,8 @@ bool TrackPanel::HitTestEnvelope(Track *track, wxRect &r, wxMouseEvent & event)
 
    // Get envelope point, range 0.0 to 1.0
    bool dB = (displayType == 1);
-   double envValue = envelope->GetValueAtX(
-      event.m_x, r, mViewInfo->h, mViewInfo->zoom );
+   // Convert x to time.
+   const double envValue = envelope->GetValue(mViewInfo->PositionToTime(event.m_x, r.x));
 
    float zoomMin, zoomMax;
    wavetrack->GetDisplayBounds(&zoomMin, &zoomMax);
