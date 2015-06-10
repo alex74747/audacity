579de3c9ef034258b1debd299cf87141095c8abe
Paul Licameli <paul.licameli@gmail.com>
2015-06-09 23:45:54 -0400

    Keep fisheye centered at the playhead during scrubbing

--- src/TrackPanel.cpp
+++ src/TrackPanel.cpp
@@ -1021,7 +1021,11 @@ void TrackPanel::OnTimer()
    }
 
 #ifdef EXPERIMENTAL_FISHEYE
-   if (mMouseCapture == IsUltraFineAdjustingFisheye &&
+   if (
+#ifdef EXPERIMENTAL_SCRUBBING_BASIC
+       !IsScrubbing() &&
+#endif
+         mMouseCapture == IsUltraFineAdjustingFisheye &&
          mViewInfo->GetFisheyeState() == ZoomInfo::PINNED) {
       wxMouseState state(::wxGetMouseState());
       wxCoord xx = state.GetX();
@@ -1099,6 +1103,7 @@ void TrackPanel::OnTimer()
 #ifdef EXPERIMENTAL_SCRUBBING_SMOOTH_SCROLL
       if (mSmoothScrollingScrub)
          // Redraw with every timer tick, to keep the indicator centered.
+         // Update fisheye in OnPaint, not here, to keep it precisely centered.
          Refresh(false);
       else
 #endif
@@ -1110,6 +1115,12 @@ void TrackPanel::OnTimer()
                // Show or hide the maximum speed.
                Refresh(false);
          }
+#ifdef EXPERIMENTAL_FISHEYE
+         if (mViewInfo->GetFisheyeState() != ZoomInfo::HIDDEN) {
+            mViewInfo->SetFisheyeCenterTime(gAudioIO->GetStreamTime());
+            RefreshFisheye();
+         }
+#endif
       }
    }
 #endif
@@ -1541,6 +1552,10 @@ void TrackPanel::OnPaint(wxPaintEvent & /* event */)
          // at higher magnifications, and keeps the green line still in the middle.
          indicator = gAudioIO->GetStreamTime();
          mViewInfo->h = indicator - mViewInfo->screen / 2.0;
+#ifdef EXPERIMENTAL_FISHEYE
+         if (mViewInfo->GetFisheyeState() != ZoomInfo::HIDDEN)
+            mViewInfo->SetFisheyeCenterTime(indicator);
+#endif
          if (!mScrollBeyondZero)
             // Can't scroll too far left
             mViewInfo->h = std::max(0.0, mViewInfo->h);
