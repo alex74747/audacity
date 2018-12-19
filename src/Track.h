e13b53b91b9ac9a4e8f228384558dddad96a3cc1
Paul Licameli <paul.licameli@audacityteam.org>
2018-12-19 12:22:12 -0500

    TrackList::EmptyRange() is static and public

--- src/Track.h
+++ src/Track.h
@@ -1234,6 +1234,7 @@ class TrackList final
          FindLeader( const_cast<Track*>(pTrack) ).Filter< const Track >();
    }
 
+   static TrackIterRange< Track > EmptyRange();
 
    template < typename TrackType = Track >
       auto Any()
@@ -1484,8 +1485,6 @@ private:
       auto e = const_cast<TrackList*>(this)->getEnd();
       return { e, e, e };
    }
-
-   TrackIterRange< Track > EmptyRange() const;
 
    bool isNull(TrackNodePointer p) const
    { return (p.second == this && p.first == ListOfTracks::end())
