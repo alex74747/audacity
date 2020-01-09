4dc673fb58272d42be41220c56297906be0f82cf
Paul Licameli <paul.licameli@audacityteam.org>
2020-01-09 13:08:49 -0500

    New class SpectralSelectHandle inheriting SelectHandle

---
+++ src/tracks/ui/SpectralSelectHandle.cpp
@@ -0,0 +1,13 @@
+/**********************************************************************
+ 
+ Audacity: A Digital Audio Editor
+ 
+ SpectralSelectHandle.cpp
+ 
+ Paul Licameli split from SelectHandle.cpp
+ 
+ **********************************************************************/
+
+#include "SpectralSelectHandle.h"
+
+SpectralSelectHandle::~SpectralSelectHandle() = default;
