6d84e555580a3f309b45b12870d442b9badbef18
Paul Licameli <paul.licameli@audacityteam.org>
2020-02-17 17:17:50 -0500

    New files for PluginIds

---
+++ src/PluginIds.cpp
@@ -0,0 +1,11 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  PluginIds.cpp
+
+  Paul Licameli split from PluginManager.cpp
+
+**********************************************************************/
+
+#include "PluginIds.h"
---
+++ src/PluginIds.h
@@ -0,0 +1,14 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  PluginIds.h
+
+  Paul Licameli split from PluginManager.h
+
+**********************************************************************/
+
+#ifndef __AUDACITY_PLUGIN_IDS__
+#define __AUDACITY_PLUGIN_IDS__
+
+#endif
