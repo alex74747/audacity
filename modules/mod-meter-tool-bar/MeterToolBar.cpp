7390bd2efeba731a93f1e14e8dff67df10ffe54e
Paul Licameli <paul.licameli@audacityteam.org>
2020-10-01 17:05:04 -0400

    Meter toolbars in a module

--- modules/mod-meter-tool-bar/MeterToolBar.cpp
+++ modules/mod-meter-tool-bar/MeterToolBar.cpp
@@ -17,7 +17,7 @@
 *//*******************************************************************/
 
 
-#include "../Audacity.h"
+#include "Audacity.h"
 #include "MeterToolBar.h"
 
 // For compilers that support precompilation, includes "wx/wx.h".
@@ -31,9 +31,9 @@
 
 #include <wx/gbsizer.h>
 
-#include "../AllThemeResources.h"
-#include "../ProjectAudioIO.h"
-#include "../widgets/Meter.h"
+#include "AllThemeResources.h"
+#include "ProjectAudioIO.h"
+#include "widgets/Meter.h"
 
 IMPLEMENT_CLASS(MeterToolBar, ToolBar);
 
@@ -287,7 +287,7 @@ static RegisteredToolbarFactory factory3{
             XO("Combined Meter"), MeterToolBar::ID() } }; }
 };
 
-#include "ToolManager.h"
+#include "toolbars/ToolManager.h"
 
 namespace {
 AttachedToolBarMenuItem sAttachment1{
@@ -312,3 +312,6 @@ AttachedToolBarMenuItem sAttachment2{
 //   { PlayMeterBarID, RecordMeterBarID }
 //};
 }
+
+#include "ModuleConstants.h"
+DEFINE_MODULE_ENTRIES
