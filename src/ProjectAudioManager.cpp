6e1e9b1691275836595ee57a1891b8c885b999c3
Paul Licameli <paul.licameli@audacityteam.org>
2020-10-04 11:14:10 -0400

    Scrubbing bar had idle handler; push notifications were not needed

--- src/ProjectAudioManager.cpp
+++ src/ProjectAudioManager.cpp
@@ -363,11 +363,6 @@ void ProjectAudioManager::Stop(bool stopStream /* = true*/)
          meter->Clear();
       }
    }
-
-   // To do: eliminate this, use an event instead
-   const auto toolbar = ToolManager::Get( *project ).GetToolBar(wxT("Scrub"));
-   if (toolbar)
-      toolbar->EnableDisableButtons();
 }
 
 void ProjectAudioManager::Pause()
