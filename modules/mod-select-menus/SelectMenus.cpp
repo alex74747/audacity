0d1e4451c7aafa107fa5a3bcc5e06b239735cb7f
Paul Licameli <paul.licameli@audacityteam.org>
2020-10-05 17:43:08 -0400

    Select menus in a module

--- modules/mod-select-menus/SelectMenus.cpp
+++ modules/mod-select-menus/SelectMenus.cpp
@@ -1,31 +1,29 @@
-
-
-#include "../AdornedRulerPanel.h"
+#include "AdornedRulerPanel.h"
 #include "AudioIO.h"
 #include "CommonCommandFlags.h"
-#include "../SpectrumAnalyst.h"
+#include "SpectrumAnalyst.h"
 #include "Prefs.h"
 #include "Project.h"
 #include "ProjectAudioIO.h"
-#include "../ProjectAudioManager.h"
+#include "ProjectAudioManager.h"
 #include "ProjectHistory.h"
 #include "ProjectRate.h"
-#include "../ProjectSelectionManager.h"
-#include "../ProjectSettings.h"
-#include "../ProjectWindow.h"
+#include "ProjectSelectionManager.h"
+#include "ProjectSettings.h"
+#include "ProjectWindow.h"
 #include "ProjectWindows.h"
-#include "../SelectUtilities.h"
+#include "SelectUtilities.h"
 #include "SyncLock.h"
-#include "../TrackPanel.h"
+#include "TrackPanel.h"
 #include "WaveTrack.h"
-#include "../LabelTrack.h"
+#include "LabelTrack.h"
 #include "CommandContext.h"
 #include "CommandManager.h"
-#include "../toolbars/ControlToolBar.h"
-#include "../tracks/ui/SelectHandle.h"
-#include "../tracks/labeltrack/ui/LabelTrackView.h"
-#include "../tracks/playabletrack/wavetrack/ui/WaveTrackView.h"
-#include "../tracks/playabletrack/wavetrack/ui/WaveTrackViewConstants.h"
+#include "toolbars/ControlToolBar.h"
+#include "tracks/ui/SelectHandle.h"
+#include "tracks/labeltrack/ui/LabelTrackView.h"
+#include "tracks/playabletrack/wavetrack/ui/WaveTrackView.h"
+#include "tracks/playabletrack/wavetrack/ui/WaveTrackViewConstants.h"
 
 // private helper classes and functions
 namespace {
@@ -1256,3 +1254,6 @@ AttachedItem sAttachment5{
 }
 
 #undef FN
+
+#include "ModuleConstants.h"
+DEFINE_MODULE_ENTRIES
