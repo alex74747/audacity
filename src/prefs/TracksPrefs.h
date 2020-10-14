fc14cafd33c3ae085c7da5e2121fefc1020e603d
Paul Licameli <paul.licameli@audacityteam.org>
2020-10-14 08:52:20 -0400

    Register some details of TracksPrefs page in other files...
    ... And so TracksPrefs does not depend on WaveformSettings

--- src/prefs/TracksPrefs.h
+++ src/prefs/TracksPrefs.h
@@ -15,10 +15,10 @@
 
 //#include <wx/defs.h>
 
+#include <functional>
 #include <vector>
 #include "PrefsPanel.h"
 #include "../tracks/playabletrack/wavetrack/ui/WaveTrackViewConstants.h"
-#include "WaveformSettings.h" // for ScaleTypeValues
 
 class ShuttleGui;
 
@@ -26,7 +26,23 @@ class ShuttleGui;
 
 class AUDACITY_DLL_API TracksPrefs final : public PrefsPanel
 {
+   struct PopulatorItem;
  public:
+   //! Type of function that adds to the Tracks preference page
+   using Populator = std::function< void(ShuttleGui&) >;
+
+   //! To be statically constructed, it registers additions to the Library preference page
+   struct AUDACITY_DLL_API RegisteredControls
+      : public Registry::RegisteredItem<PopulatorItem>
+   {
+      RegisteredControls( const Identifier &id,
+         unsigned section, //!< 0 for checkmarks section, 1 for choices
+         Populator populator,
+         const Registry::Placement &placement = { wxEmptyString, {} } );
+
+      struct AUDACITY_DLL_API Init{ Init(); };
+   };
+
    TracksPrefs(wxWindow * parent, wxWindowID winid);
    ~TracksPrefs();
    ComponentInterfaceSymbol GetSymbol() override;
@@ -43,17 +59,31 @@ class AUDACITY_DLL_API TracksPrefs final : public PrefsPanel
    
    static wxString GetDefaultAudioTrackNamePreference();
 
+   static const wxChar *WaveformScaleKey();
+   static const wxChar *DBValueString();
+
    static Identifier ViewModeChoice();
-   static WaveformSettings::ScaleTypeValues WaveformScaleChoice();
    static WaveTrackViewConstants::SampleDisplay SampleViewChoice();
    static WaveTrackViewConstants::ZoomPresets Zoom1Choice();
    static WaveTrackViewConstants::ZoomPresets Zoom2Choice();
 
  private:
+   struct AUDACITY_DLL_API PopulatorItem final : Registry::SingleItem {
+      static Registry::GroupItem &Registry();
+   
+      PopulatorItem(
+         const Identifier &id, unsigned section, Populator populator);
+
+      unsigned mSection;
+      Populator mPopulator;
+   };
    void Populate();
    void PopulateOrExchange(ShuttleGui & S) override;
 
    static int iPreferencePinned;
 };
+
+// Guarantees registry exists before attempts to use it
+static TracksPrefs::RegisteredControls::Init sInitRegisteredControls;
 
 #endif
