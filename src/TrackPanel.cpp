b844271cc2cd446acf4ad2e54fac85ad5014cd0b
Paul Licameli <paul.licameli@audacityteam.org>
2020-10-16 16:37:14 -0400

    Invert dependency of ProjectManager on TrackPanel

--- src/TrackPanel.cpp
+++ src/TrackPanel.cpp
@@ -55,6 +55,7 @@ is time to refresh some aspect of the screen.
 #include "ProjectAudioIO.h"
 #include "ProjectAudioManager.h"
 #include "ProjectHistory.h"
+#include "ProjectManager.h"
 #include "ProjectSettings.h"
 #include "ProjectStatus.h"
 #include "ProjectWindow.h"
@@ -215,6 +216,16 @@ AudacityProject::AttachedWindows::RegisteredFactory sKey{
    }
 };
 
+struct FactoryInstaller {
+   FactoryInstaller() {
+      prevFactory = ProjectManager::InstallMainPanelFactory(
+         [](AudacityProject &project, wxWindow*, wxWindowID) -> CellularPanel& {
+            return TrackPanel::Get(project); });
+   }
+   ~FactoryInstaller() { ProjectManager::InstallMainPanelFactory(prevFactory); }
+   ProjectManager::MainPanelFactory prevFactory;
+} factoryInstaller;
+
 }
 
 TrackPanel &TrackPanel::Get( AudacityProject &project )
@@ -227,13 +238,10 @@ const TrackPanel &TrackPanel::Get( const AudacityProject &project )
    return Get( const_cast< AudacityProject & >( project ) );
 }
 
-void TrackPanel::Destroy( AudacityProject &project )
+bool TrackPanel::Destroy()
 {
-   auto *pPanel = project.AttachedWindows::Find( sKey );
-   if (pPanel) {
-      pPanel->wxWindow::Destroy();
-      project.AttachedWindows::Assign( sKey, nullptr );
-   }
+   GetProject()->AttachedProjectWindows::Assign(sKey, nullptr);
+   return CellularPanel::Destroy();
 }
 
 // Don't warn us about using 'this' in the base member initializer list.
