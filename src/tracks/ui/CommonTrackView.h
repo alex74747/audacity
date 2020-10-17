4d27e17ffbf297abb1e35ba0ab576b1c07413beb
Paul Licameli <paul.licameli@audacityteam.org>
2020-10-17 03:36:40 -0400

    CommonTrackView does not depend on BackgroundCell

--- src/tracks/ui/CommonTrackView.h
+++ src/tracks/ui/CommonTrackView.h
@@ -16,6 +16,7 @@ Paul Licameli split from class TrackView
 
 class SelectHandle;
 class TimeShiftHandle;
+class ZoomHandle;
 
 class AUDACITY_DLL_API CommonTrackView /* not final */ : public TrackView
 {
@@ -53,6 +54,7 @@ protected:
 
 public:
    std::weak_ptr<TimeShiftHandle> mTimeShiftHandle;
+   mutable std::weak_ptr<ZoomHandle> mZoomHandle;
 };
 
 #endif
