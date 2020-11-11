7fc741404896ec808408a141c72075e66110085a
Paul Licameli <paul.licameli@audacityteam.org>
2020-11-11 09:40:45 -0500

    New functions for some MIDI-only steps in AudioIO & AudioIOBase

--- src/AudioIOBase.cpp
+++ src/AudioIOBase.cpp
@@ -350,6 +350,13 @@ bool AudioIOBase::IsBusy() const
    return false;
 }
 
+#ifdef EXPERIMENTAL_MIDI_OUT
+bool AudioIOBase::IsOtherStreamActive() const
+{
+   return ( mMidiStreamActive && !mMidiOutputComplete );
+}
+#endif
+
 bool AudioIOBase::IsStreamActive() const
 {
    bool isActive = false;
@@ -358,8 +365,7 @@ bool AudioIOBase::IsStreamActive() const
       isActive = (Pa_IsStreamActive( mPortStreamV19 ) > 0);
 
 #ifdef EXPERIMENTAL_MIDI_OUT
-   if( mMidiStreamActive && !mMidiOutputComplete )
-      isActive = true;
+   isActive = isActive || IsOtherStreamActive();
 #endif
    return isActive;
 }
