5bba0ec8f8c0b053ce74f20e9e106ea2b9fdabbd
Paul Licameli <paul.licameli@audacityteam.org>
2020-11-13 00:00:26 -0500

    Move some menu items out of FileMenus.cpp...
    ... Either into ExportMultiple.cpp, or eliminated as duplicated in modules

--- src/ProjectFileManager.cpp
+++ src/ProjectFileManager.cpp
@@ -34,7 +34,6 @@ Paul Licameli split from AudacityProject.cpp
 #include "UndoManager.h"
 #include "WaveTrack.h"
 #include "wxFileNameWrapper.h"
-#include "export/Export.h"
 #include "toolbars/SelectionBar.h"
 #include "widgets/AudacityMessageBox.h"
 #include "widgets/ErrorDialog.h"
