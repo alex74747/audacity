b622e2bc55d7ed4b77aec8b2d72aa85a6926044a
Paul Licameli <paul.licameli@audacityteam.org>
2020-11-16 18:05:07 -0500

    Remove WaveTrack::GetClipAtSample, just use WaveTrack::GetIntervals

--- src/WaveTrack.cpp
+++ src/WaveTrack.cpp
@@ -1964,20 +1964,6 @@ void WaveTrack::GetEnvelopeValues(double *buffer, size_t bufferLen,
    }
 }
 
-WaveClip* WaveTrack::GetClipAtSample(sampleCount sample)
-{
-   for (const auto &clip: mClips)
-   {
-      auto start = clip->GetStartSample();
-      auto len   = clip->GetNumSamples();
-
-      if (sample >= start && sample < start + len)
-         return clip.get();
-   }
-
-   return NULL;
-}
-
 // When the time is both the end of a clip and the start of the next clip, the
 // latter clip is returned.
 WaveClip* WaveTrack::GetClipAtTime(double time)
