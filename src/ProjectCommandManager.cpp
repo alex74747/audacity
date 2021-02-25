37cf3cd730aad702267a629fba532293bd4c74c0
Paul Licameli <paul.licameli@audacityteam.org>
2021-02-25 10:24:45 -0500

    Journal stuff

--- src/ProjectCommandManager.cpp
+++ src/ProjectCommandManager.cpp
@@ -10,7 +10,12 @@
 
 #include "ProjectCommandManager.h"
 #include "commands/CommandManager.h"
+#include "ActiveProject.h"
+#include "JournalRegistry.h"
+#include "Menus.h"
 #include "Project.h"
+#include "commands/CommandContext.h"
+#include "commands/CommandTargets.h"
 
 void ProjectCommandManager::UpdateCheckmarksInAllProjects()
 {
@@ -18,4 +23,31 @@ void ProjectCommandManager::UpdateCheckmarksInAllProjects()
       auto &project = *pProject;
       CommandManager::Get(project).UpdateCheckmarks(project);
    }
+}
+
+namespace {
+
+constexpr auto JournalCode = wxT("CM");  // for CommandManager
+
+// Register a callback for the journal
+Journal::RegisteredCommand sCommand{ JournalCode,
+[]( const wxArrayString &fields )
+{
+   // Expect JournalCode and the command name.
+   // To do, perhaps, is to include some parameters.
+   bool handled = false;
+   if ( fields.size() == 2 ) {
+      if (auto project = GetActiveProject().lock()) {
+         auto pManager = &CommandManager::Get( *project );
+         auto flags = MenuManager::Get( *project ).GetUpdateFlags();
+         const CommandContext context( *project );
+         auto &command = fields[1];
+         handled =
+            pManager->HandleTextualCommand( command, context, flags, false );
+      }
+   }
+   return handled;
+}
+};
+
 }
