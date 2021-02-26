169c0b9761d61b36be6674b3593592bf759f94f2
Paul Licameli <paul.licameli@audacityteam.org>
2021-02-26 11:07:14 -0500

    Most of Edit menu in a module (excluding Undo, Redo, Preferences)

--- src/prefs/ModulePrefs.cpp
+++ src/prefs/ModulePrefs.cpp
@@ -233,6 +233,7 @@ static const std::unordered_set<wxString> &autoEnabledModules()
       "mod-note-track",
       "mod-keyboard-preferences",
       "mod-time-ruler",
+      "mod-basic-editing",
    };
    return modules;
 }
