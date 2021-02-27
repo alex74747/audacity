6d88c63dc80b08327e49a98fec4d5e958dae03b1
Paul Licameli <paul.licameli@audacityteam.org>
2021-02-27 15:43:55 -0500

    ModuleManager does not depend on ModulePrefs

---
+++ src/ModuleSettings.cpp
@@ -0,0 +1,154 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  @file ModuleSettings.cpp
+
+  Paul Licameli split from ModulePrefs.cpp
+
+**********************************************************************/
+
+#include "ModuleSettings.h"
+#include "FileNames.h"
+#include <unordered_set>
+
+static const std::unordered_set<wxString> &autoEnabledModules()
+{
+   // Add names to this list, of modules that are expected to ship
+   // with Audacity and enable automatically.
+   static std::unordered_set<wxString> modules{
+      "mod-mixer-board",
+      "mod-lyrics",
+      "mod-undo-history",
+      "mod-contrast",
+      "mod-frequency-plot",
+      "mod-timer-record",
+      "mod-clip-menu-items",
+      "mod-track-menus",
+      "mod-select-menus",
+      "mod-view-menus",
+      "mod-help-menus",
+      "mod-navigation-menus",
+      "mod-audiounits",
+      "mod-ladspa",
+      "mod-lv2",
+      "mod-nyquist",
+      "mod-vamp",
+      "mod-vst",
+      "mod-command-classes",
+      "mod-screenshot",
+      "mod-printing",
+      "mod-macros",
+      "mod-plugin-menus",
+      "mod-flac",
+      "mod-ogg",
+      "mod-mp2",
+      "mod-ffmpeg",
+      "mod-mp3",
+      "mod-pcm",
+      "mod-midi-import-export",
+      "mod-import-export",
+      "mod-tags-ui",
+      "mod-tags",
+      "mod-soundtouch",
+      "mod-effect-classes",
+      "mod-effects",
+      "mod-transcription-tool-bar",
+      "mod-device-tool-bar",
+      "mod-mixer-tool-bar",
+      "mod-edit-tool-bar",
+      "mod-meter-tool-bar",
+      "mod-tools-tool-bar",
+      "mod-scrubbing-tool-bar",
+      "mod-time-tool-bar",
+      "mod-spectral-selection-tool-bar",
+      "mod-selection-tool-bar",
+      "mod-control-tool-bar",
+      "mod-note-track-ui",
+      "mod-time-track-ui",
+      "mod-label-track-ui",
+      "mod-waveform-ui",
+      "mod-spectrum-ui",
+      "mod-wave-track-ui",
+      "mod-playable-track-ui",
+      "mod-common-track-ui",
+      "mod-scripting",
+      "mod-edit-cursor-overlay",
+      "mod-play-indicator-overlay",
+      "mod-label-track",
+      "mod-time-track",
+      "mod-note-track",
+      "mod-keyboard-preferences",
+      "mod-time-ruler",
+      "mod-basic-editing",
+      "mod-toolbars",
+   };
+   return modules;
+}
+
+// static function that tells us about a module.
+int ModuleSettings::GetModuleStatus(const FilePath &fname)
+{
+   // Default status is NEW module, and we will ask once.
+   int iStatus = kModuleNew;
+
+   wxFileName FileName( fname );
+   wxString ShortName = FileName.GetName().Lower();
+
+   wxString PathPref = wxString( wxT("/ModulePath/") ) + ShortName;
+   wxString StatusPref = wxString( wxT("/Module/") ) + ShortName;
+   wxString DateTimePref = wxString( wxT("/ModuleDateTime/") ) + ShortName;
+
+   wxString ModulePath = gPrefs->Read( PathPref, wxEmptyString );
+   if( ModulePath.IsSameAs( fname ) )
+   {
+      gPrefs->Read( StatusPref, &iStatus, kModuleNew );
+
+      wxDateTime DateTime = FileName.GetModificationTime();
+      wxDateTime OldDateTime;
+      OldDateTime.ParseISOCombined( gPrefs->Read( DateTimePref, wxEmptyString ) );
+
+      // Some platforms return milliseconds, some do not...level the playing field
+      DateTime.SetMillisecond( 0 );
+      OldDateTime.SetMillisecond( 0 );
+
+      // fix up a bad status or reset for newer module
+      if( iStatus > kModuleNew || !OldDateTime.IsEqualTo( DateTime ) )
+      {
+         iStatus=kModuleNew;
+      }
+   }
+   else
+   {
+      // Remove previously saved since it's no longer valid
+      gPrefs->DeleteEntry( PathPref );
+      gPrefs->DeleteEntry( StatusPref );
+      gPrefs->DeleteEntry( DateTimePref );
+   }
+
+   if (iStatus == kModuleNew) {
+      if (autoEnabledModules().count(ShortName))
+         iStatus = kModuleEnabled;
+   }
+
+   return iStatus;
+}
+
+void ModuleSettings::SetModuleStatus(const FilePath &fname, int iStatus)
+{
+   wxFileName FileName( fname );
+   wxDateTime DateTime = FileName.GetModificationTime();
+   wxString ShortName = FileName.GetName().Lower();
+
+   wxString PrefName = wxString( wxT("/Module/") ) + ShortName;
+   gPrefs->Write( PrefName, iStatus );
+
+   PrefName = wxString( wxT("/ModulePath/") ) + ShortName;
+   gPrefs->Write( PrefName, fname );
+
+   PrefName = wxString( wxT("/ModuleDateTime/") ) + ShortName;
+   gPrefs->Write( PrefName, DateTime.FormatISOCombined() );
+
+   gPrefs->Flush();
+}
+
