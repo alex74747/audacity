4edd4fa560a697518f39e2d50da599001e042816
Paul Licameli <paul.licameli@audacityteam.org>
2021-03-14 20:25:11 -0400

    ProjectFileIO does not depend on ProgressDialog

--- src/ProjectFileIO.cpp
+++ src/ProjectFileIO.cpp
@@ -24,6 +24,7 @@ Paul Licameli split from AudacityProject.cpp
 #include "ActiveProjects.h"
 #include "CodeConversions.h"
 #include "DBConnection.h"
+#include "FileNames.h"
 #include "Project.h"
 #include "ProjectSerializer.h"
 #include "ProjectWindows.h"
@@ -33,7 +34,6 @@ Paul Licameli split from AudacityProject.cpp
 #include "WaveTrack.h"
 #include "widgets/AudacityMessageBox.h"
 #include "BasicUI.h"
-#include "widgets/ProgressDialog.h"
 #include "wxFileNameWrapper.h"
 #include "XMLFileReader.h"
 #include "SentryHelper.h"
@@ -989,6 +989,8 @@ bool ProjectFileIO::CopyTo(const FilePath &destpath,
    bool prune /* = false */,
    const std::vector<const TrackList *> &tracks /* = {} */)
 {
+   using namespace BasicUI;
+
    auto pConn = CurrConn().get();
    if (!pConn)
       return false;
@@ -1142,7 +1144,8 @@ bool ProjectFileIO::CopyTo(const FilePath &destpath,
 
       /* i18n-hint: This title appears on a dialog that indicates the progress
          in doing something.*/
-      ProgressDialog progress(XO("Progress"), msg, pdlgHideStopButton);
+      auto progress =
+         BasicUI::MakeProgress(XO("Progress"), msg, ProgressShowCancel);
       ProgressResult result = ProgressResult::Success;
 
       wxLongLong_t count = 0;
@@ -1199,7 +1202,7 @@ bool ProjectFileIO::CopyTo(const FilePath &destpath,
             THROW_INCONSISTENCY_EXCEPTION;
          }
 
-         result = progress.Update(++count, total);
+         result = progress->Poll(++count, total);
          if (result != ProgressResult::Success)
          {
             // Note that we're not setting success, so the finally
