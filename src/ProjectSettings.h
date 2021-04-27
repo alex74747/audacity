d455d63a75a5007a4047d6c1d1d0755c343468e8
Paul Licameli <paul.licameli@audacityteam.org>
2021-04-27 08:56:57 -0400

    Remove Time Shift tool (and bind f5 not f6 to Multi tool)

--- src/ProjectSettings.h
+++ src/ProjectSettings.h
@@ -36,7 +36,6 @@ enum {
    envelopeTool,
    drawTool,
    zoomTool,
-   slideTool,
    multiTool,
    numTools,
    
--- src/toolbars/ToolsToolBar.cpp
+++ src/toolbars/ToolsToolBar.cpp
@@ -80,7 +80,6 @@ ToolsToolBar::ToolsToolBar( AudacityProject &project )
    //Read the following wxASSERTs as documentating a design decision
    wxASSERT( selectTool   == selectTool   - firstTool );
    wxASSERT( envelopeTool == envelopeTool - firstTool );
-   wxASSERT( slideTool    == slideTool    - firstTool );
    wxASSERT( zoomTool     == zoomTool     - firstTool );
    wxASSERT( drawTool     == drawTool     - firstTool );
    wxASSERT( multiTool    == multiTool    - firstTool );
@@ -146,7 +145,6 @@ void ToolsToolBar::RegenerateTooltips()
    } table[] = {
       { selectTool,   wxT("SelectTool"),    XO("Selection Tool")  },
       { envelopeTool, wxT("EnvelopeTool"),  XO("Envelope Tool")   },
-      { slideTool,    wxT("TimeShiftTool"), XO("Time Shift Tool") },
       { zoomTool,     wxT("ZoomTool"),      XO("Zoom Tool")       },
       { drawTool,     wxT("DrawTool"),      XO("Draw Tool")       },
       { multiTool,    wxT("MultiTool"),     XO("Multi-Tool")      },
@@ -202,7 +200,6 @@ void ToolsToolBar::Populate()
    mTool[ envelopeTool ] = MakeTool( this, bmpEnvelope, envelopeTool, XO("Envelope Tool") );
    mTool[ drawTool     ] = MakeTool( this, bmpDraw, drawTool, XO("Draw Tool") );
    mTool[ zoomTool     ] = MakeTool( this, bmpZoom, zoomTool, XO("Zoom Tool") );
-   mTool[ slideTool    ] = MakeTool( this, bmpTimeShift, slideTool, XO("Slide Tool") );
    mTool[ multiTool    ] = MakeTool( this, bmpMulti, multiTool, XO("Multi-Tool") );
 
    // It's OK to reset the tool when regenerating this, e.g after visiting preferences.
@@ -359,12 +356,6 @@ void OnZoomTool(const CommandContext &context)
    SetTool(context.project, ToolCodes::zoomTool);
 }
 
-/// Handler to set the Time shift tool active
-void OnTimeShiftTool(const CommandContext &context)
-{
-   SetTool(context.project, ToolCodes::slideTool);
-}
-
 void OnMultiTool(const CommandContext &context)
 {
    SetTool(context.project, ToolCodes::multiTool);
@@ -421,10 +412,8 @@ BaseItemSharedPtr ExtraToolsMenu()
          AlwaysEnabledFlag, wxT("F3") ),
       Command( wxT("ZoomTool"), XXO("&Zoom Tool"), FN(OnZoomTool),
          AlwaysEnabledFlag, wxT("F4") ),
-      Command( wxT("TimeShiftTool"), XXO("&Time Shift Tool"),
-         FN(OnTimeShiftTool), AlwaysEnabledFlag, wxT("F5") ),
       Command( wxT("MultiTool"), XXO("&Multi Tool"), FN(OnMultiTool),
-         AlwaysEnabledFlag, wxT("F6") ),
+         AlwaysEnabledFlag, wxT("F5") ),
       Command( wxT("PrevTool"), XXO("&Previous Tool"), FN(OnPrevTool),
          AlwaysEnabledFlag, wxT("A") ),
       Command( wxT("NextTool"), XXO("&Next Tool"), FN(OnNextTool),
