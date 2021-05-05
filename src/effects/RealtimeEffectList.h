25e20a68e52a32a5f558a20867a1a4d000b2e56b
Leland Lucius <github@homerow.net>
2021-05-05 01:51:35 -0500

    Introduce per-project and per-track RealtimeEffectList objects

---
+++ src/effects/RealtimeEffectList.h
@@ -0,0 +1,36 @@
+/**********************************************************************
+
+ Audacity: A Digital Audio Editor
+
+ RealtimeEffectList.h
+
+ *********************************************************************/
+
+#ifndef __AUDACITY_REALTIMEEFFECTLIST_H__
+#define __AUDACITY_REALTIMEEFFECTLIST_H__
+
+#include "TrackAttachment.h"
+
+class AudacityProject;
+
+class Track;
+
+class RealtimeEffectList final
+   : public TrackAttachment
+   , public std::enable_shared_from_this<RealtimeEffectList>
+{
+   RealtimeEffectList(const RealtimeEffectList &) = delete;
+   RealtimeEffectList &operator=(const RealtimeEffectList &) = delete;
+
+public:
+   RealtimeEffectList();
+   virtual ~RealtimeEffectList();
+
+   static RealtimeEffectList &Get(AudacityProject &project);
+   static const RealtimeEffectList &Get(const AudacityProject &project);
+
+   static RealtimeEffectList &Get(Track &track);
+   static const RealtimeEffectList &Get(const Track &track);
+};
+
+#endif // __AUDACITY_REALTIMEEFFECTLIST_H__
