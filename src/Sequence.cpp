9be9af6afe88fab26a8fd789275f1c42fca35f6f
Paul Licameli <paul.licameli@audacityteam.org>
2021-05-30 12:08:09 -0400

    Sequence updates effective format when samples are pasted into it...
    ... Making the effective format the wider of the previous and new materials'
    effective formats.
    
    That is, if either old or new material would require dithering to another
    format, assume all material will require it.
    
    We won't track the distinction more finely than for whole clips.
    
    Check all the return paths of Sequence::Paste().

--- src/Sequence.cpp
+++ src/Sequence.cpp
@@ -541,6 +541,7 @@ void Sequence::Paste(sampleCount s, const Sequence *src)
 
       CommitChangesIfConsistent
          (newBlock, samples, wxT("Paste branch one"));
+      mSampleFormats.UpdateEffective(src->mSampleFormats.Effective());
       return;
    }
 
@@ -589,6 +590,7 @@ void Sequence::Paste(sampleCount s, const Sequence *src)
       // This consistency check won't throw, it asserts.
       // Proof that we kept consistency is not hard.
       ConsistencyCheck(wxT("Paste branch two"), false);
+      mSampleFormats.UpdateEffective(src->mSampleFormats.Effective());
       return;
    }
 
@@ -675,6 +677,8 @@ void Sequence::Paste(sampleCount s, const Sequence *src)
 
    CommitChangesIfConsistent
       (newBlock, mNumSamples + addedLen, wxT("Paste branch three"));
+
+   mSampleFormats.UpdateEffective(src->mSampleFormats.Effective());
 }
 
 /*! @excsafety{Strong} */
